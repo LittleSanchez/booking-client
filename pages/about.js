905b8539ab664a96085e419d97784e04166a754f
богдан хлипавка <bodyabandan@mail.ru>
2021-02-25 22:44:42 +0200

    'initial'

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+
+
+function About() {
+    return <>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus provident neque corrupti? Maxime architecto quod, vel velit, doloremque aperiam officiis, laborum eos fugit dicta odit quisquam commodi provident! Eum id officiis fuga sequi, quisquam eos, illum debitis consequuntur veritatis atque sapiente mollitia ex unde ipsa in molestias quod! Magni molestias itaque animi nulla in corporis sit dolore repudiandae dolorem reprehenderit pariatur dignissimos saepe ea impedit dolorum, quae eaque illum? Sed laboriosam assumenda ea nobis voluptas in fuga atque? Ullam nulla nostrum optio numquam accusamus repudiandae temporibus recusandae id? Culpa, praesentium! Quae aliquid consequatur, deserunt, pariatur facere ipsam omnis magni error sed laudantium dolorum quas hic ex atque minima tempora, nemo quia? Hic, autem illo. Libero aperiam, magni facilis similique, eius consequatur, itaque quae quas reprehenderit enim ullam laudantium incidunt rerum. Voluptatem veritatis quasi ipsum ratione delectus praesentium quidem harum vero, similique excepturi mollitia. Est, officiis architecto ipsam ipsa error repudiandae cumque quas temporibus laboriosam tenetur libero dolor labore enim, beatae neque provident alias, voluptatem ipsum ratione corporis cupiditate! Saepe itaque illo quidem possimus quam iste veritatis iure ex quos maxime, eveniet tempora quis esse id officiis, eaque iusto eius voluptatum hic? Numquam eveniet qui est, repudiandae exercitationem odit nemo minima!</p>
+    </>
+}
+
+export default About;
